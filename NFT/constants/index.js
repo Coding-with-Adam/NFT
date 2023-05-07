5b5b8d7585f06ef9c1e447bb91502038087c16d6
adam schroeder <adamschroeder.m@gmail.com>
2023-05-07 03:02:08 -0400

    first commit

---
+++ NFT/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed in previous whitelist Dapp
+const WHITELIST_CONTRACT_ADDRESS = "0xa729B8215A5a536b03182767dbFF59005AcD32A7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
